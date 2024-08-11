cd554b183bf7c38510ddc317bf6f82ef64e0ccf3
Vincent Swarte <vincentswarte@gmail.com>
2024-08-11 03:38:06 +0200

    Update 20240711184420_add_group_password_tables.sql

--- crates/database/migrations/20240711184420_add_group_password_tables.sql
+++ crates/database/migrations/20240711184420_add_group_password_tables.sql
@@ -1,12 +1,12 @@
 BEGIN;
 
 ALTER TABLE bloodmessages
-ADD COLUMN group_passwords TEXT[];
+ADD COLUMN group_passwords TEXT[] NOT NULL;
 
 ALTER TABLE bloodstains
-ADD COLUMN group_passwords TEXT[];
+ADD COLUMN group_passwords TEXT[] NOT NULL;
 
 ALTER TABLE ghostdata
-ADD COLUMN group_passwords TEXT[];
+ADD COLUMN group_passwords TEXT[] NOT NULL;
 
 COMMIT;
