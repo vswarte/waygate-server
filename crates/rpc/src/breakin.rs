74d42b93067422fba89099a3569a5a7bd4e27393
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2025-01-26 02:52:17 +0300

    shuffle breakin targets

--- crates/rpc/src/breakin.rs
+++ crates/rpc/src/breakin.rs
@@ -2,9 +2,12 @@ use rand::prelude::*;
 
 use waygate_connection::send_push;
 use waygate_connection::ClientSession;
-use waygate_pool::BREAKIN_POOL;
-use waygate_pool::breakin::BreakInPoolQuery;
 use waygate_message::*;
+use waygate_pool::breakin::BreakInPoolQuery;
+use waygate_pool::BREAKIN_POOL;
+
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 
 use crate::HandlerResult;
 
@@ -13,11 +16,12 @@ pub async fn handle_get_break_in_target_list(
     params: RequestGetBreakInTargetListParams,
 ) -> HandlerResult {
     let play_region = params.play_region;
-    let entries = BREAKIN_POOL
+    let mut entries = BREAKIN_POOL
         .match_entries::<BreakInPoolQuery>(&params.into())
         .iter()
         .map(|m| m.into())
         .collect::<Vec<_>>();
+    entries.shuffle(&mut thread_rng());
 
     Ok(ResponseParams::GetBreakInTargetList(
         ResponseGetBreakInTargetListParams {
@@ -48,11 +52,9 @@ pub async fn handle_break_in_target(
         }),
     });
 
-    Ok(
-        send_push(request.player_id, push_payload)
-            .await
-            .map(|_| ResponseParams::BreakInTarget)?
-    )
+    Ok(send_push(request.player_id, push_payload)
+        .await
+        .map(|_| ResponseParams::BreakInTarget)?)
 }
 
 /// Sent by the host after receiving a `BreakInTarget` push to share the
@@ -73,11 +75,9 @@ pub async fn handle_allow_break_in_target(
         }),
     });
 
-    Ok(
-        send_push(request.player_id, push_payload)
-            .await
-            .map(|_| ResponseParams::AllowBreakInTarget)?
-    )
+    Ok(send_push(request.player_id, push_payload)
+        .await
+        .map(|_| ResponseParams::AllowBreakInTarget)?)
 }
 
 /// Sent by the host to reject a `BreakInTarget` push. This might happen
@@ -100,9 +100,7 @@ pub async fn handle_reject_break_in_target(
         }),
     });
 
-    Ok(
-        send_push(request.invading_player_id, push_payload)
-            .await
-            .map(|_| ResponseParams::RejectBreakInTarget)?
-    )
+    Ok(send_push(request.invading_player_id, push_payload)
+        .await
+        .map(|_| ResponseParams::RejectBreakInTarget)?)
 }
